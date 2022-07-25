4dcbc101692f4a6bbf0a77011bedf83d313d0c6e
Lion <84781313+Lion127@users.noreply.github.com>
2022-07-26 01:46:56 +0200

    Add files via upload

---
+++ Test Programme/Buzzer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+int pin = 27;
+using var controller = new GpioController();
+controller.OpenPin(pin, PinMode.Output);
+while (true)
+{
+    controller.Write(pin, PinValue.High );
+    Thread.Sleep(500);
+    controller.Write(pin, PinValue.Low );
+    Thread.Sleep(500);
+}
