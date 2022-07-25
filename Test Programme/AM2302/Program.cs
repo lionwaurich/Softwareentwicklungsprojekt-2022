eed62c80791976b442115d30f6038fc63d017543
Lion <84781313+Lion127@users.noreply.github.com>
2022-07-26 01:45:13 +0200

    Add files via upload

---
+++ Test Programme/AM2302/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Iot.Device.DHTxx;
+
+using (Dht22 dht = new Dht22(4))
+
+while(true)
+{   
+    Console.WriteLine($"{dht.Humidity}°C");
+    Console.WriteLine($"{dht.Temperature}%");
+    Thread.Sleep(1000);
+}
