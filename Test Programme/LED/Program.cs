5a95d7819bbe7fadb4d035304a4e6163a20f6aee
Lion <84781313+Lion127@users.noreply.github.com>
2022-07-26 01:43:35 +0200

    Add files via upload

---
+++ Test Programme/LED/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+Console.WriteLine("Blinken");
+int Rot = 22;
+int Grün = 23;
+using var controller = new GpioController();
+controller.OpenPin(Rot, PinMode.Output);
+controller.OpenPin(Grün, PinMode.Output);
+while (true)
+{
+    controller.Write(Rot, PinValue.Low);
+    controller.Write(Grün, PinValue.Low);
+    Thread.Sleep(1000);
+   
+}
